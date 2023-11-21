f457be549ff2f1a63cdf0212a1fe6dc624b4e299
YasunaCoffee <yasunaiwase@gmail.com>
2023-11-22 00:11:18 +0900

    set auth

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,11 +1,16 @@
 import basicAuth from 'basic-auth';
 
 export default function handler(req, res) {
-  const credentials = basicAuth(req);
+  try {
+    const credentials = basicAuth(req);
 
-  if (credentials && credentials.name === process.env.BASIC_AUTH_USERNAME && credentials.pass === process.env.BASIC_AUTH_PASSWORD) {
-    // 認証が成功した場合、次のミドルウェアまたはルートハンドラーに進みます。
-    // ここでは、単に成功のレスポンスを送ります。
-    res.status(200).json({ status: 'Authenticated' });
+    if (credentials && credentials.name === process.env.BASIC_AUTH_USERNAME && credentials.pass === process.env.BASIC_AUTH_PASSWORD) {
+      res.status(200).json({ status: 'Authenticated' });
+    } else {
+      res.status(401).end('Access denied');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).end('Internal server error');
   }
 }
