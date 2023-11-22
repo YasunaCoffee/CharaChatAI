dcbd411ddad91d80e34d3ae0607e374ab1b60494
YasunaCoffee <yasunaiwase@gmail.com>
2023-11-22 14:22:31 +0900

    set meta

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,10 +1,10 @@
 import { buildUrl } from "@/utils/buildUrl";
 import Head from "next/head";
 export const Meta = () => {
-  const title = "ChatVRM";
+  const title = "きゃらチャットAI";
   const description =
-    "Webブラウザだけで3Dキャラクターとの会話を、マイクやテキスト入力、音声合成を用いて楽しめます。キャラクター（VRM）の変更や性格設定、音声調整もできます。";
-  const imageUrl = "https://pixiv.github.io/ChatVRM/ogp.png";
+    "キャラクターとの会話を、マイクやテキスト入力、音声合成を用いて楽しめます。";
+  const imageUrl = "https://github.com/YasunaCoffee/CharaChatAI/blob/main/public/ogp.png?raw=true";
   return (
     <Head>
       <title>{title}</title>
