3e3b5e27a1c6bc7940a286a5ec93252f7a8404c1
YasunaCoffee <yasunaiwase@gmail.com>
2023-11-22 22:25:13 +0900

    fix redirect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import "@charcoal-ui/icons";
+import { Auth0Provider } from '@auth0/auth0-react';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ""}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ""}
+      onRedirectCallback={() => window.location.origin}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
+
+export default MyApp;
+
