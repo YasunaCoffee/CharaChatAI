f531b8354f882a3e0f5b9c7ab9072d5c920d679c
YasunaCoffee <yasunaiwase@gmail.com>
2023-11-22 23:17:36 +0900

    fix window

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -15,8 +15,10 @@ export default function LoginPage() {
     <Auth0Provider
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ""}
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ""}
-      authorizationParams={{
-        redirect_uri: window.location.origin
+      onRedirectCallback={() => {
+        if (typeof window !== 'undefined') {
+          window.location.href = "https://chara-chat-ai.vercel.app/"
+        }
       }}
     >
     </Auth0Provider>
