6f0c3e100c5e2507dfa6cac3e53a9d40ce67901e
YasunaCoffee <yasunaiwase@gmail.com>
2023-11-23 23:02:53 +0900

    add loginpage

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,26 +1,24 @@
-// import React from 'react';
-// import { useAuth0 } from '@auth0/auth0-react';
-// import { useRouter } from 'next/router';
-// import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useRouter } from 'next/router';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-// export default function LoginPage() {
-//   const { loginWithRedirect } = useAuth0();
-//   const router = useRouter();
+export default function LoginPage() {
+  const { loginWithRedirect } = useAuth0();
+  const router = useRouter();
 
-//   React.useEffect(() => {
-//     loginWithRedirect();
-//   }, []);
+  React.useEffect(() => {
+    loginWithRedirect();
+  }, []);
 
-//   return (
-//     <Auth0Provider
-//       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ""}
-//       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ""}
-//       onRedirectCallback={() => {
-//         if (typeof window !== 'undefined') {
-//           return window.location.origin;
-//         }
-//       }}
-//     >
-//     </Auth0Provider>
-//   );
-// }
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ""}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ""}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+    </Auth0Provider>
+  );
+}
